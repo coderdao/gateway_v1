b1b144c7549327124821f0f3ff36b55b359f2e50
abo <abo2013@foxmail.com>
2021-06-09 23:47:46 +0800

    up

---
+++ calc.go
@@ -0,0 +1,6 @@
+// calc.go
+package gateway_v1
+
+func add(num1 int, num2 int) int {
+	return num1 + num2
+}
---
+++ calc_test.go
@@ -0,0 +1,9 @@
+package gateway_v1
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	if ans := add(1, 2); ans != 3 {
+		t.Error("add(1, 2) should be equal to 3")
+	}
+}
