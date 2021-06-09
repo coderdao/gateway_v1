a3dfc1b2e07df62d5b98f98bee3374113e1abd82
abo <abo2013@foxmail.com>
2021-06-09 23:52:35 +0800

    up

---
+++ _pratise/demo/demo.go
@@ -0,0 +1,6 @@
+package demo
+
+// 根据长宽获取面积
+func GetArea(weight int, height int) int {
+	return weight * height
+}
---
+++ _pratise/demo/demo_test.go
@@ -0,0 +1,8 @@
+package demo
+import "testing"
+func TestGetArea(t *testing.T) {
+	area := GetArea(40, 50)
+	if area != 2000 {
+		t.Error("测试失败")
+	}
+}
