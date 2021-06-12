ae082973b4e260e469a8a4f505b1bdf5fdd63d0c
abo <abo2013@foxmail.com>
2021-06-12 17:54:41 +0800

    'up'

--- pratise/proxy/reverse_proxy/real_server/main.go
+++ pratise/proxy/reverse_proxy/real_server/main.go
@@ -11,57 +11,69 @@ import (
 	"time"
 )
 
-func main() {
-	rs1 := &RealServer{Addr: "127.0.0.1:2003"}
+func main()  {
+	// 设置真实服务器
+	rs1 := &RealServer{Addr:"127.0.0.1:2003"}
 	rs1.Run()
-	rs2 := &RealServer{Addr: "127.0.0.1:2004"}
+
+	rs2 := &RealServer{Addr:"127.0.0.1:2004"}
 	rs2.Run()
 
-	//监听关闭信号
+	// 监听服务关闭信号
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 }
 
+/** 服务器设置 */
 type RealServer struct {
 	Addr string
 }
 
-func (r *RealServer) Run() {
-	log.Println("Starting httpserver at " + r.Addr)
+func (r *RealServer) Run()  {
+	log.Println("starting httpserver at "+r.Addr)
+
+	// 启动路由，设置路由规则 => 对应处理方法
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", r.HelloHandler)
 	mux.HandleFunc("/base/error", r.ErrorHandler)
-	mux.HandleFunc("/test_http_string/test_http_string/aaa", r.TimeoutHandler)
+	mux.HandleFunc("/base/timeout", r.TimeoutHandler)
+
 	server := &http.Server{
-		Addr:         r.Addr,
-		WriteTimeout: time.Second * 3,
-		Handler:      mux,
+		Addr:              r.Addr,
+		WriteTimeout:      time.Second * 3,
+		Handler: 			mux,
 	}
 
-	// 协程方式调起，意味不会阻塞
 	go func() {
 		log.Fatal(server.ListenAndServe())
 	}()
 }
 
 func (r *RealServer) HelloHandler(w http.ResponseWriter, req *http.Request) {
-	//127.0.0.1:8008/abc?sdsdsa=11
-	//r.Addr=127.0.0.1:8008
-	//req.URL.Path=/abc
-	//fmt.Println(req.Host)
-	upath := fmt.Sprintf("http://%s%s\n", r.Addr, req.URL.Path)	// 直接打印请求地址
-	realIP := fmt.Sprintf("RemoteAddr=%s,X-Forwarded-For=%v,X-Real-Ip=%v\n", req.RemoteAddr, req.Header.Get("X-Forwarded-For"), req.Header.Get("X-Real-Ip"))
+	/**
+	127.0.0.1:8080/abc?sdsdsa=11
+	r.Addr=127.0.0.1:8080
+	req.URL.Path=/abc
+	 */
+
+	// 设置转发参数
+	upath := fmt.Sprintf("http://%s%s\n", r.Addr, req.URL.Path) // 打印请求地址
+	realIP := fmt.Sprintf(
+		"RemoteAddr=%s,X-Forwarded-For=%v,X-Real-Ip=%v\n",
+		req.RemoteAddr, req.Header.Get("X-Forwarded-For"), req.Header.Get("X-Real-Ip"))
 	header:=fmt.Sprintf("headers =%v\n",req.Header)
+
+	// 写入返回体 response
 	io.WriteString(w, upath)
 	io.WriteString(w, realIP)
 	io.WriteString(w, header)
-
 }
 
 func (r *RealServer) ErrorHandler(w http.ResponseWriter, req *http.Request) {
 	upath := "error handler"
 	w.WriteHeader(500)
+
 	io.WriteString(w, upath)
 }
 
